b57f81f6ab9491f9721466e914d180d8deb30c26
hrabia87 <60814071+hrabia87@users.noreply.github.com>
2020-02-08 12:21:21 +0100

    Update stopnie celsjusza.py

--- stopnie celsjusza.py
+++ stopnie celsjusza.py
@@ -1,5 +1,5 @@
 print('Policzmy temperature Farenheita')
-temp_c = int(input('Podaj stopnie Celsjusza: '))
-temp_f = 9/5*temp_c+32
-print('temperatura w Farenheitach to:', temp_f)
-input('enter')
+tempC = int(input('Podaj stopnie Celsjusza: '))
+tempF = 9/5*tempC+32
+print('temperatura w Farenheitach to:', tempF)
+input('Nacisnij enter.')
